3ee6dd28a7444e8d6ece2a3e8b46d7d1a7a91969
Xcel <mystik2002@gmail.com>
2021-03-05 09:37:23 -0500

    Added Drawer for Email

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 
+import clsx from 'clsx';
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
+import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+
 import {
     AppBar,
     Badge,
@@ -18,6 +29,46 @@ const useStyles = makeStyles(styles);
 
 export default function Header() {
     const classes = useStyles();
+    const [state, setState] = React.useState({
+        right: false,
+    });
+
+    const toggleDrawer = (anchor, open) => (event) => {
+        if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+          return;
+        }
+    
+        setState({ ...state, [anchor]: open });
+      };
+
+      const list = (anchor) => (
+        <div
+          className={clsx(classes.list, {
+            [classes.fullList]: anchor === 'top' || anchor === 'bottom',
+          })}
+          role="presentation"
+          onClick={toggleDrawer(anchor, false)}
+          onKeyDown={toggleDrawer(anchor, false)}
+        >
+          <List>
+            {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+              <ListItem button key={text}>
+                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItem>
+            ))}
+          </List>
+          <Divider />
+          <List>
+            {['All mail', 'Trash', 'Spam'].map((text, index) => (
+              <ListItem button key={text}>
+                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItem>
+            ))}
+          </List>
+        </div>
+      );
 
     return (
         <React.Fragment>
@@ -28,7 +79,19 @@ export default function Header() {
                     </Typography>
                     <IconButton color="inherit">
                         <Badge badgeContent={9} color="secondary">
-                            <EmailTwoToneIcon />
+                            {['right'].map((anchor) => (
+                            <React.Fragment key={anchor}>
+                            <EmailTwoToneIcon onClick={toggleDrawer(anchor, true)}>{anchor}</EmailTwoToneIcon>
+                            <SwipeableDrawer
+                                anchor={anchor}
+                                open={state[anchor]}
+                                onClose={toggleDrawer(anchor, false)}
+                                onOpen={toggleDrawer(anchor, true)}
+                            >
+                                {list(anchor)}
+                            </SwipeableDrawer>
+                            </React.Fragment>
+                            ))}
                         </Badge>
                     </IconButton>
                     <IconButton color="inherit">
