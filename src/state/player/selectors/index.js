5f72880d940af0ca159aea76476e9fc555a882a2
Simon Germain <simon@pps.io>
2021-04-19 23:40:42 -0400

    Added canAfford selector

--- src/state/player/selectors/index.js
+++ src/state/player/selectors/index.js
@@ -2,4 +2,5 @@ import common from '../../_common/selectors/_common';
 
 export default common.bindToReducer('player', {
     currency: common.get('currency'),
+    canAfford: amount => state => state.currency >= amount,
 });
