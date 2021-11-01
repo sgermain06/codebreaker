152b4d80c860219df368ef89b85e493611b71bd5
Simon Germain <simon@prth.com>
2021-11-01 15:51:11 -0400

    Added dark web bg

--- src/pages/darkWeb/styles.js
+++ src/pages/darkWeb/styles.js
@@ -1,8 +1,14 @@
+import background from '../../assets/dark_web_bg.png';
+
 export default function styles(theme) {
     return {
         container: {
             padding: theme.spacing(2),
-            textAlign: 'center'
+            textAlign: 'center',
+            background: `linear-gradient(rgba(0,0,0,0.35), rgba(0,0,0,0.35)), url(${background})`,
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+            width: '100%',
         },
         button: {
             margin: 8,
