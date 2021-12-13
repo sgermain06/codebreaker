95f772cb7f2d0f008abe9a6411411460bac7b818
Simon Germain <simon@prth.com>
2021-12-13 00:08:04 -0500

    Fixed array of loaders spacing

--- src/lib/terminalApps/http.js
+++ src/lib/terminalApps/http.js
@@ -10,7 +10,7 @@ module.exports = class HttpRequest {
     async run(url, ...args) {
         this.terminal.stdout(`Requesting GET from ${url}...`);
         // const loader = [ ' ⠷', ' ⠯', ' ⠟', ' ⠻', ' ⠽', ' ⠾' ];
-        const loader = [ ' [o---]', '[-o--]', '[--o-]', '[---o]', '[--o-]', '[-o--]' ];
+        const loader = [ ' [o---]', ' [-o--]', ' [--o-]', ' [---o]', ' [--o-]', ' [-o--]' ];
         return await this.terminal.withLoader(async () => {
             await new Promise(resolve => setTimeout(resolve, 1000));
             const response = await axios.get(`${$config.endpoint}/api/v1/request?url=${url}`);
